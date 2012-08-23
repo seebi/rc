19a6c59fc4bf46afff37cbe1b00e5b820a66e8d5
Sebastian Tramp <mail@sebastian.tramp.name>
2012-08-23 20:31:28 +0200

    initial version with zsh, ssh, git and vim support

---
+++ init/init.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#
+# This scripts creates an minimal linux home environment so I feel comfortable
+# on servers
+#
+
+# needed packages
+packages="zsh git exuberant-ctags"
+username="seebi"
+
+if [ "$USER" = "root" ]; then
+    apt-get update
+    apt-get upgrade
+    apt-get install $packages
+    useradd -m -s `which zsh` $username
+fi
+
+if [ "$USER" = "$username" ]; then
+
+    # xdb directories
+    mkdir -p ~/.config/ ~/.cache/
+
+    # clone and run
+    # this will setup git, ssh, vim, zsh, ...
+    rcdir=~/.config/rc
+    rm -rf $rcdir
+    git clone git://github.com/seebi/rc.git $rcdir
+    cd $rcdir/rc
+    make all
+fi
