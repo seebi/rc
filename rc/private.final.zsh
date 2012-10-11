241fad7259db31221a99dffdd8d8801fc7bbb72f
Sebastian Tramp <mail@sebastian.tramp.name>
2012-10-11 19:38:45 +0200

    add vars for dch

--- rc/private.final.zsh
+++ rc/private.final.zsh
@@ -1,3 +1,7 @@
+
+export DEBFULLNAME="Sebastian Tramp"
+export DEBEMAIL="tramp@informatik.uni-leipzig.de"
+
 # start a screen/tmux session if not already in it
 if [[ "$TERM" =~ "^screen.*$" ]]; then
     # I'm already inside of a screen
