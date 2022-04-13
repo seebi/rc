5509f28459ecd229af75fd47c531cf6b1d8b0e39
Sebastian Tramp <mail@sebastian.tramp.name>
2022-04-13 13:27:32 +0200

    add missing package

--- init/init.sh
+++ init/init.sh
@@ -6,7 +6,7 @@
 #
 
 # needed packages
-packages="make zsh git exuberant-ctags byobu exa neovim byobu"
+packages="make zsh git exuberant-ctags byobu exa neovim byobu exa"
 
 sudo apt update
 sudo apt upgrade
